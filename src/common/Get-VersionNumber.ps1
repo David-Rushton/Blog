922107e8aceeffd7a42a5328b222b4a60e8f681c
David Rushton <david-rushton@outlook.com>
2020-07-01 21:40:24 +0100

    Added script for getting the build number

---
+++ src/common/Get-VersionNumber.ps1
@@ -0,0 +1,11 @@
+Set-StrictMode -Version 'Latest'
+
+$versionPath = Join-Path -Path $PSScriptRoot -ChildPath 'version.xml'
+$versionXml = Select-Xml -Path $versionPath -XPath '/version'
+
+$major = $versionXml.Node.major
+$minor = $versionXml.Node.minor
+$buildNumber = $env:GITHUB_RUN_ID
+$shortSha = git rev-parse --short HEAD
+
+return "$major.$minor.$buildNumber.$shortSha"
