eeeaf3979027865257265fac37faed74522ac54d
David Rushton <david-rushton@outlook.com>
2020-07-04 23:44:01 +0100

    All version numbers now come from the same script

--- src/common/Get-VersionNumber.ps1
+++ src/common/Get-VersionNumber.ps1
@@ -1,11 +1,29 @@
+[CmdletBinding()]
+param(
+    [switch]$IncludeSha
+)
+
 Set-StrictMode -Version 'Latest'
 
 $versionPath = Join-Path -Path $PSScriptRoot -ChildPath 'version.xml'
 $versionXml = Select-Xml -Path $versionPath -XPath '/version'
+
+
+if(-not $env:GITHUB_RUN_ID) {
+    $env:GITHUB_RUN_ID = "local-build"
+}
+
 
 $major = $versionXml.Node.major
 $minor = $versionXml.Node.minor
 $buildNumber = $env:GITHUB_RUN_ID
 $shortSha = git rev-parse --short HEAD
 
-return "$major.$minor.$($buildNumber):$shortSha"
+$versionNumber = "v$major.$minor.$($buildNumber)"
+
+if($IncludeSha) {
+    $versionNumber = "$versionNumber $shortSha"
+}
+
+
+return $versionNumber
