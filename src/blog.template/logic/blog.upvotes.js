8b0819b37ee74a3bfea530250ab49c0b7e9d4f66
David Rushton <david-rushton@outlook.com>
2020-07-13 23:00:52 +0100

    Upvotes logic moved to its own file
    Not all pages use this feature

---
+++ src/blog.template/logic/blog.upvotes.js
@@ -0,0 +1,38 @@
+/**
+ * controls upvote behaviour
+ */
+(enableUpvotes = async () => {
+
+    const id = document.querySelector('head[data-article-id').getAttribute('data-article-id');
+    const upvoteTag = document.querySelector('#article-up-vote-count');
+    let upvoteScore = 0;
+
+    const makeApiCall = async (method) => await fetch(`/api/v1/upvotes/${id}`, { method: method });
+    const updateDisplay = () => upvoteTag.innerText = upvoteScore.toLocaleString();
+
+
+    // query db for current upvote score
+    const articleRecord = await makeApiCall('GET');
+    if( ! articleRecord.ok ) {
+        console.error(`Cannot retrieve current upvote score.  Id: ${id}`);
+        return;
+    }
+
+    // read value and display to user
+    const articleJson = await articleRecord.json();
+    upvoteScore = articleJson.article.upVotes;
+    updateDisplay();
+
+
+    // event handler upvote button
+    document.querySelector('#article-up-vote').onclick = async function() {
+
+        // update display first - appears faster to user
+        upvoteScore++;
+        updateDisplay();
+
+        let response = await makeApiCall('POST');
+        if( ! response.ok )
+            console.error(`Cannot upvote article.  Id: ${id}`);
+    };
+})();
